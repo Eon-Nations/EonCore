15a22fec33f9f0b124c78e80523c0f7aaec4e08f
Colin Grebenstein <72942847+colin-grebenstein@users.noreply.github.com>
2022-01-01 10:08:03 -0800

    Removed script and simplified decryption command

--- src/main/resources/decrypt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Script from the Github Docs
-
-mkdir private-key
-gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPTION_PASSWORD" --output /private-key/action-key src/main/resources/action-key.gpg
