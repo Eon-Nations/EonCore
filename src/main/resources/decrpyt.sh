c4c79dd7d0b75053dcd6530c6081472be53ad1ef
Colin Grebenstein <72942847+colin-grebenstein@users.noreply.github.com>
2022-01-01 08:05:39 -0800

    Got rid of the $HOME variable

--- src/main/resources/decrpyt.sh
+++ src/main/resources/decrpyt.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Script from the Github Docs
 
-mkdir $HOME/private-key
-gpg --quiet --batch --yes --decrpyt --passphrase="$DECRYPTION_PASSWORD" --output $HOME/private-key/action-key $HOME/.github/action-key.gpg
+mkdir private-key
+gpg --quiet --batch --yes --decrpyt --passphrase="$DECRYPTION_PASSWORD" --output /private-key/action-key src/main/action-key.gpg
