f1af4b27a5adb165f26c2dfc246bf1df9ed63b66
colin-grebenstein <72942847+colin-grebenstein@users.noreply.github.com>
2022-04-19 11:05:53 -0400

    Added TestCommandSpyCommand to further test commands

--- src/main/java/me/squid/eoncore/commands/CommandSpyCommand.java
+++ src/main/java/me/squid/eoncore/commands/CommandSpyCommand.java
@@ -11,24 +11,24 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Consumer;
 
 public class CommandSpyCommand implements CommandExecutor, Listener {
     EonCore plugin;
-
-    private final List<Player> peopleSpying = new ArrayList<>();
+    private final Set<Player> peopleSpying = new HashSet<>();
 
     public CommandSpyCommand(EonCore plugin) {
         this.plugin = plugin;
         plugin.getCommand("commandspy").setExecutor(this);
+        Bukkit.getPluginManager().registerEvents(this, plugin);
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player p) {
-            if (!peopleSpying.contains(p)){
+            if (!peopleSpying.contains(p)) {
                 peopleSpying.add(p);
                 p.sendMessage(Utils.chat(plugin.getConfig().getString("CommandSpy-On")));
             } else {
@@ -40,20 +40,16 @@ public class CommandSpyCommand implements CommandExecutor, Listener {
     }
 
     @EventHandler
-    public void onCommandSend(PlayerCommandPreprocessEvent e){
+    public void onCommandSend(PlayerCommandPreprocessEvent e) {
         Player p = e.getPlayer();
         String cmd = e.getMessage();
+        String commandMessage = Utils.chat(plugin.getConfig().getString("CommandSpy-Message")
+                .replace("<player>", p.getName()).replace("<command>", cmd));
+        Consumer<Player> sendCommandMessage = player -> player.sendMessage(commandMessage);
 
         if (!p.hasPermission(getImmunePerm())) {
-            Stream<? extends Player> playerStream = Bukkit.getOnlinePlayers().stream();
-            playerStream.filter(peopleSpying::contains)
-                        .forEach(player -> player.sendMessage(Utils.chat(plugin.getConfig().getString("CommandSpy-Message")
-                            .replace("<player>", p.getName()).replace("<command>", cmd))));
+            peopleSpying.forEach(sendCommandMessage);
         }
-    }
-
-    public String getPermissionNode(){
-        return "eoncommands.commandspy";
     }
 
     public String getImmunePerm(){
---
+++ src/test/java/commands/TestCommandSpyCommand.java
@@ -0,0 +1,58 @@
+package commands;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+import be.seeseemelk.mockbukkit.entity.PlayerMock;
+import me.squid.eoncore.EonCore;
+import org.bukkit.Bukkit;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class TestCommandSpyCommand {
+
+    private ServerMock server;
+
+    @Before
+    public void setUp() {
+        server = MockBukkit.mock();
+        MockBukkit.createMockPlugin("LuckPerms");
+        server.addSimpleWorld("spawn_void");
+        MockBukkit.load(EonCore.class);
+    }
+
+    private void callCommandEvent(PlayerMock player, String command) {
+        PlayerCommandPreprocessEvent preprocessEvent = new PlayerCommandPreprocessEvent(player, command);
+        Bukkit.getPluginManager().callEvent(preprocessEvent);
+    }
+
+    @Test
+    @DisplayName("Players cannot access commandspy")
+    public void testNoPerms() {
+        PlayerMock player = server.addPlayer();
+        PlayerMock other = server.addPlayer();
+        player.performCommand("commandspy");
+        callCommandEvent(other, "/hat");
+        other.assertNoMoreSaid();
+    }
+
+    @Test
+    @DisplayName("OP Player are succesfully toggled")
+    public void testToggle() {
+        PlayerMock player = server.addPlayer();
+        player.setOp(true);
+        player.performCommand("commandspy");
+        player.nextMessage();
+        PlayerMock dummy = server.addPlayer("Dummy");
+        callCommandEvent(dummy, "/help");
+        player.assertSaid("§7[§6§lCommandSpy§r§7] Dummy: /help");
+    }
+
+    @After
+    public void tearDown() {
+        MockBukkit.unmock();
+    }
+
+}
