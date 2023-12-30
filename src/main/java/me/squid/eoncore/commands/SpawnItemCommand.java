2e08607c6c9894c798f683b117d7d90e69ada79f
SerenityCodes <72942847+SerenityCodes@users.noreply.github.com>
2023-12-30 02:53:02 +0000

    Implemented floating items for nodes

--- src/main/java/me/squid/eoncore/commands/SpawnItemCommand.java
+++ src/main/java/me/squid/eoncore/commands/SpawnItemCommand.java
@@ -2,29 +2,28 @@ package me.squid.eoncore.commands;
 
 import me.squid.eoncore.EonCommand;
 import me.squid.eoncore.EonCore;
+import me.squid.eoncore.holograms.FloatingItem;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.util.Vector;
 import org.eonnations.eonpluginapi.api.Command;
 
 @Command(name = "spawnitem",
         usage = "/spawnitem <material>")
 public class SpawnItemCommand extends EonCommand {
 
+    EonCore plugin;
+
     public SpawnItemCommand(EonCore plugin) {
         super(plugin);
+        this.plugin = plugin;
     }
 
     @Override
     protected void execute(Player player, String[] args) {
         Material material = Material.valueOf(args[0]);
-        ItemStack itemStack = new ItemStack(material, 1);
-        Item item = player.getWorld().dropItem(player.getLocation(), itemStack);
-        item.setVelocity(new Vector(0, 0, 0));
-        item.setCanPlayerPickup(false);
-        item.setGlowing(true);
-        item.setGravity(false);
+        FloatingItem item = new FloatingItem(player.getLocation(), material);
+        Bukkit.getScheduler().runTaskLater(plugin, () -> item.close(), 300L);
     }
 }
