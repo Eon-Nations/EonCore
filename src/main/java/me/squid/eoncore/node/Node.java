ddf3c479d7e8d935a6c0adc54ba0572851f01582
SerenityCodes <72942847+SerenityCodes@users.noreply.github.com>
2024-04-08 16:06:15 +0000

    Changed some of Node

--- src/main/java/me/squid/eoncore/node/Node.java
+++ src/main/java/me/squid/eoncore/node/Node.java
@@ -2,9 +2,12 @@ package me.squid.eoncore.node;
 
 import me.squid.eoncore.holograms.FloatingItem;
 import me.squid.eoncore.holograms.Hologram;
+import me.squid.eoncore.misc.menus.MenuBuilder;
 
 import java.util.Random;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import io.vavr.collection.List;
 
@@ -15,9 +18,15 @@ import org.bukkit.entity.ArmorStand;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.util.Vector;
 import org.eonnations.eonpluginapi.events.EventSubscriber;
 import org.eonnations.eonpluginapi.events.EventHandler;
+import org.eonnations.eonpluginapi.menu.Item;
+import org.eonnations.eonpluginapi.menu.Menu;
+
+import com.mysql.cj.util.StringUtils;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextReplacementConfig;
@@ -28,14 +37,17 @@ public class Node {
     
     private static final int MIN_OUTPUT_RATE = 10;
     private static final int MAX_OUTPUT_RATE = 100;
-    private static EventHandler<BlockPlaceEvent> chestPlace = registerChestPlaceListener();
     private static EventHandler<BlockBreakEvent> deepslateCancel = cancelBreakingDeepslate();    
-    private static EventHandler<BlockBreakEvent> replaceHolo = replaceUnclaimed();
+    private static EventHandler<PlayerInteractEvent> menuInteraction = nodeMenuListener();
 
     private Hologram nodeInfo;
     private FloatingItem resourceDisplay;
     private int outputRate;
     private boolean isClaimed;
+    private Location location;
+    private Resource resource;
+
+    private static Map<Location, Node> cache = new HashMap<>();
 
     private static boolean claimNode(BlockPlaceEvent event) {
         Collection<ArmorStand> holograms = event.getPlayer().getLocation().getNearbyEntitiesByType(ArmorStand.class, 5.0);
@@ -55,31 +67,32 @@ public class Node {
             .handler(e -> true);
     }
 
-    private static boolean claimToUnclaim(BlockBreakEvent e) {
-        Collection<ArmorStand> holograms = e.getPlayer().getLocation().getNearbyEntitiesByType(ArmorStand.class, 5.0);
+    private static EventHandler<PlayerInteractEvent> nodeMenuListener() {
+        if (menuInteraction != null) return menuInteraction;
+        return EventSubscriber.subscribe(PlayerInteractEvent.class, EventPriority.NORMAL)
+            .filter(e -> e.getClickedBlock().getType().equals(Material.REINFORCED_DEEPSLATE))
+            .filter(e -> e.getAction().equals(Action.RIGHT_CLICK_BLOCK))
+            .handler(e -> {
+                Node currentNode = cache.get(e.getClickedBlock().getLocation());
+                // Add names for the chest and ender chest respectively
+                Menu menu = Menu.create(27, Component.text(currentNode.getResource().name() + " Node - " + currentNode.getOutputRate() + "/min"))
+                    .withItem(11, Item.builder(Material.CHEST).finish())
+                    .withItem(15, Item.builder(Material.ENDER_CHEST).finish())
+                    .makeDummySlots()
+                    .complete();
+                menu.openToPlayer(e.getPlayer());
+                return false;
+            });
+    }
+
+    private static void claimToUnclaim(Location loc) {
+        Collection<ArmorStand> holograms = loc.getNearbyEntitiesByType(ArmorStand.class, 5.0);
         for (ArmorStand stand : holograms) {
             Component name = stand.customName().replaceText(TextReplacementConfig.builder().match("CLAIMED").replacement("UNCLAIMED").build());
             stand.customName(name);
         }
-        return false;
     }
 
-    private static EventHandler<BlockBreakEvent> replaceUnclaimed() {
-        if (replaceHolo != null) return replaceHolo;
-        return EventSubscriber.subscribe(BlockBreakEvent.class, EventPriority.NORMAL)
-            .filter(e -> e.getBlock().getType().equals(Material.CHEST) && 
-                e.getBlock().getRelative(BlockFace.DOWN).getType().equals(Material.REINFORCED_DEEPSLATE))
-            .handler(Node::claimToUnclaim);
-    }
-
-    public static EventHandler<BlockPlaceEvent> registerChestPlaceListener() {
-        if (chestPlace != null) return chestPlace;
-        return EventSubscriber.subscribe(BlockPlaceEvent.class, EventPriority.NORMAL)  
-            .filter(e -> e.getBlock().getType().equals(Material.CHEST))
-            .filter(e -> e.getBlockAgainst().getType().equals(Material.REINFORCED_DEEPSLATE))
-            .handler(Node::claimNode);
-    }
-    
     public Node(Location location, Resource resource) {
         Random random = new Random();
         this.outputRate = random.nextInt((MAX_OUTPUT_RATE - MIN_OUTPUT_RATE) + 1) + MIN_OUTPUT_RATE;
@@ -97,8 +110,20 @@ public class Node {
             fromFormatString("<#D55E00><bold>" + claimString + "</bold></#D55E00>"));
         Location nodeLoc = location.clone().add(new Vector(0, 1, 0));
         this.nodeInfo = new Hologram(lines, nodeLoc);
+        cache.put(location, this);
     }
 
+    public Resource getResource() {
+        return resource;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public int getOutputRate() {
+        return outputRate;
+    }
 
     public void remove() {
         nodeInfo.close();
